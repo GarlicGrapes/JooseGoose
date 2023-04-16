975555dcb1f1f5a129637988d067a346633cf9cc
Austin Longo <austinlongocontracting@gmail.com>
2023-04-16 09:04:36 -0400

    generated posts model and controller, added 2 seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+OnEarth = Post.create(title:"On Earth We're Briefly Gorgeous", body:"Lord I don't cry no more, don't look to the sky no more")
+AllAboutLove = Post.create(title:"All About Love", body:"That is not love, that is cathexis")

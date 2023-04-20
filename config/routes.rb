3f83239871cc98e9a68286ab312a3eb6071b1945
Austin Longo <austinlongocontracting@gmail.com>
2023-04-20 11:28:27 -0400

    sessions functional

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
   resources :users, only: [:create, :show]
   resources :sessions, only: [:create, :destroy]
+
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+  post '/signup', to: "users#create"
+  get "/auth", to: "users#show"
+  get '/me', to: "users#show"
+  
 end

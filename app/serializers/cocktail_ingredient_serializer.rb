b807915650b0afa0f7b2e4da1b67a4231353fd7f
Austin <austinlongocontracting@gmail.com>
2023-05-30 12:10:49 -0400

    removed unnecessary method in cocktail-ingredient model

--- app/serializers/cocktail_ingredient_serializer.rb
+++ app/serializers/cocktail_ingredient_serializer.rb
@@ -2,10 +2,4 @@ class CocktailIngredientSerializer < ActiveModel::Serializer
   attributes :quantity, :unit, :ingredient
   belongs_to :ingredient, serializer: IngredientNameSerializer
 
-  def ingredientName
-    ingredient_id = self.ingredient_id
-    Ingredient.find(ingredient_id).name
-  end
-
-
 end
